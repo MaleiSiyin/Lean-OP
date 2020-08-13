42799193802d9fa7f4b81cb0be4495959639bcc9
马雷斯银 <40496272+MaleiSiyin@users.noreply.github.com>
2020-08-14 03:31:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,13 +13,14 @@
 #修改版本号
 sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/SakuraTv/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/12.12.12.12/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/12.12.12.13/g' package/base-files/files/bin/config_generate
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除部分软件包
 rm -rf package/lean/luci-theme-argon
 #下载其他软件包
-mkdir /package/sakura
-cd /package/sakura
+cd /lede/package
+mkdir /sakura
+cd /sakura
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/destan19/OpenAppFilter && mv -f OpenAppFilter/* ./
 git clone https://github.com/tty228/luci-app-serverchan
